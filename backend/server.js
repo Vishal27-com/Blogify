ccfda95f7eba04d0372b34c31da8d300c7ccfa50
Vishal27-com <vishalgupta962148@gmail.com>
2023-04-14 13:19:37 +0530

    cookie added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const cors =require("cors")
+const cookieParser=require("cookie-parser")
 const dbConnect=require("./db")
 const userRouter=require("./Routes/users.routes")
 const authRouter=require("./Routes/auth.routes")
 const blogRouter=require("./Routes/blog.routes")
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(cors())
+const corsConfig={
+    origin:['http://localhost:8080','*'],
+    allowedHeaders:['Content-Type','Authorization','x-csrf-token'],
+    credentials:true,
+    exposedHeaders:['*','Authorization']
+}
+app.use(cors(corsConfig))
+app.use(cookieParser())
 dbConnect()
 app.get('/', (req, res) => res.send('hello'))
 app.use('/auth',authRouter)
